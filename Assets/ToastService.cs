27551c3f5904943333c6856e0336399ddbca6cbc
David Arutiunian <arutunjan666@mail.com>
2021-01-09 18:41:29 +0300

    🚸 Minor UX improvements

---
+++ Assets/ToastService.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class ToastService : MonoBehaviour
+{
+    void OnEnable()
+    {
+        Application.logMessageReceived += ShowAndroidToastMessage;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= ShowAndroidToastMessage;
+    }
+
+    /// <summary>
+    /// Show an Android toast message.
+    /// </summary>
+    /// <param name="message">Message string to show in the toast.</param>
+    private void ShowAndroidToastMessage(string message, string stacktrace, LogType type)
+    {
+        var player = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        var activity = player.GetStatic<AndroidJavaObject>("currentActivity");
+
+        if (activity != null)
+        {
+            var toastClass = new AndroidJavaClass("android.widget.Toast");
+            activity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+            {
+                var toastObject = toastClass.CallStatic<AndroidJavaObject>("makeText", activity, message, 0);
+                toastObject.Call("show");
+            }));
+        }
+    }
+}
